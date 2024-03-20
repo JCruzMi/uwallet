6f685e6c297a155ccbb681b69e1878ad9158fa66
Huan <juan.murato.cruz@gmail.com>
2024-03-20 16:30:50 -0500

    midle

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,48 @@
+import { sql } from "@vercel/postgres";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password", placeholder: "*****" },
+      },
+      async authorize(credentials, req) {
+        console.log(credentials);
+
+        const userFound = await sql`
+        SELECT * 
+        FROM users 
+        WHERE email = ${`%${credentials.email}%`}`;
+
+        if (!userFound) throw new Error("No user found");
+
+        console.log(userFound);
+
+        const matchPassword = await bcrypt.compare(
+          credentials.password,
+          userFound.password
+        );
+
+        if (!matchPassword) throw new Error("Wrong password");
+
+        return {
+          id: userFound.id,
+          name: userFound.username,
+          email: userFound.email,
+        };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,11 @@
-export default function Login() {
+import { GetUsers } from '@/lib/users';
+
+// #region Functions (1)
+
+export default async function Login() {
+  const users = await GetUsers()
+  console.log(users)
   return "Login"
 }
+
+// #endregion Functions (1)
