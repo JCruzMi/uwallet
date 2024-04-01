3fbbbf260754e54ab9cec200da3cd7be34db899e
Huan <juan.murato.cruz@gmail.com>
2024-04-01 17:45:27 -0500

    update dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,44 +1,37 @@
 import SectionButtons from "@/components/SectionButtons";
 import SectionSend from "@/components/SectionSend";
-import Slider from "@/components/Slider";
+import SliderCards from "@/components/SliderCards";
+
+// #region Functions (1)
 
 export default function Dashboard() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-between p-4">
-      <div className="z-10 max-w-5xl items-center justify-center font-mono text-sm lg:flex">
-        <section className="flex flex-col gap-4">
-          <div className="flex gap-4 max-w-5xl">
-            <div className="min-h-40 min-w-24 rounded-lg bg-slate-100"></div>
-            <div className="flex-grow max-w-[calc(100%-112px)]">
-              <Slider />
-            </div>
-          </div>
-
-          <div className="flex justify-center">
-            <SectionButtons />
-          </div>
-
-          <div className="flex flex-col gap-4 w-full">
-            <div className="flex justify-between">
-              <h1>Send money</h1>
-              <h1>View all</h1>
-            </div>
-            <div className="flex-grow max-w-5xl w-full">
-              <SectionSend />
-            </div>
-          </div>
-
+    <main className="flex min-h-screen flex-col items-center justify-between lg:p-24 p-4 w-full">
+      <div className="z-10 max-w-5xl w-full items-start justify-start font-mono text-sm gap-4 flex flex-col">
+        <div className="flex flex-col gap-0">
+          <p className="text-sm font-light">Total Balance</p>
+          <p className="text-2xl font-semibold">$ 7.000.000</p>
+        </div>
+        <div className="flex flex-row w-full gap-4 overflow-x-hidden">
           <div className="flex flex-col gap-4">
-            <div className="flex justify-between">
-              <h1>Movements</h1>
-              <h1>View all</h1>
-            </div>
-            <div className="flex-grow">
-              {/* Componente de movimientos */}
-            </div>
+            <div className="bg-white w-[62px] rounded-lg h-full"></div>
+            <div className="bg-white w-[62px] rounded-lg h-full"></div>
           </div>
-        </section>
+          <SliderCards />
+        </div>
+        <SectionButtons />
+        <div className="flex justify-between w-full">
+          <h1>Send money</h1>
+          <h1>View all</h1>
+        </div>
+        <SectionSend />
+        <div className="flex justify-between w-full">
+          <h1>Movements</h1>
+          <h1>View all</h1>
+        </div>
       </div>
     </main>
   );
 }
+
+// #endregion Functions (1)
