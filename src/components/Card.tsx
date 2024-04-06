5c82c6df5b16423814c34217c60f2a37a0f6078f
Kiara <fernandezperezkiara@gmail.com>
2024-04-06 11:27:34 -0500

    atributos card y avatar
    está buena la música

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,10 @@
 // #region Type aliases (1)
 
 type Card = {
-  amount?: number;
   name?: string;
+  title?: string;
+  amount?: number;
+  numWallet?: string;
   category?: string;
 };
 
@@ -10,7 +12,13 @@ type Card = {
 
 // #region Functions (1)
 
-export default function Card({ amount, name, category }: Card) {
+export default function Card({
+  name,
+  title,
+  amount,
+  numWallet,
+  category,
+}: Card) {
   return (
     <div className="max-w-[230px] min-w-[230px] w-full min-h-[140px] rounded-lg bg-gradient-to-r from-pink-300 to-purple-500"></div>
   );
