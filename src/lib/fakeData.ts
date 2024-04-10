0ce7bde17056a84cfa94aeb830e2ad90acb4b83b
Kiara <fernandezperezkiara@gmail.com>
2024-04-10 18:29:07 -0500

    fakeData

---
+++ src/lib/fakeData.ts
@@ -0,0 +1,34 @@
+const cardData = [
+  {
+    name: "John Doe",
+    amount: 500,
+    numWallet: "1234 5678 9012 3456",
+    category: "Tech",
+  },
+  {
+    name: "Jane Smith",
+    amount: 700,
+    numWallet: "9876 5432 1098 7654",
+    category: "Marketing",
+  },
+  {
+    name: "Alice Johnson",
+    amount: 600,
+    numWallet: "2468 1357 8024 6790",
+    category: "Design",
+  },
+  {
+    name: "Bob Brown",
+    amount: 450,
+    numWallet: "1357 2468 6790 8024",
+    category: "Sales",
+  },
+  {
+    name: "Eva Garcia",
+    amount: 550,
+    numWallet: "8024 6790 2468 1357",
+    category: "HR",
+  },
+];
+
+export default cardData;
