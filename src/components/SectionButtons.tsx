d0915af616ab9a1be70cdc05fa7ef529a244f5f7
Huan <juan.murato.cruz@gmail.com>
2024-04-10 18:42:22 -0500

    update

--- src/components/SectionButtons.tsx
+++ src/components/SectionButtons.tsx
@@ -1,14 +1,16 @@
-import { Button } from './ui/Button';
+import { Button } from "./ui/Button";
 
 // #region Functions (1)
 
 export default function SectionButtons() {
-  return <div className="flex gap-4 w-full max-w-sm">
-    <Button>Send</Button>
-    <Button>Receive</Button>
-    <Button>Withdraw</Button>
-    <Button>Others</Button>
-  </div>
+  return (
+    <div className="flex gap-2 w-full max-w-sm">
+      <Button>Send</Button>
+      <Button>Receive</Button>
+      <Button>Withdraw</Button>
+      <Button>Others</Button>
+    </div>
+  );
 }
 
 // #endregion Functions (1)
--- src/components/dashboard/Navbar.tsx
+++ src/components/dashboard/Navbar.tsx
@@ -1,29 +1,33 @@
 import {
-  HomeIcon,
-  UserIcon,
   ArrowsRightLeftIcon,
   BellIcon,
+  HomeIcon,
   QrCodeIcon,
+  UserIcon,
 } from "@heroicons/react/24/outline";
+
+// #region Functions (1)
 
 export default function Navbar() {
   return (
-    <div className="bg-zinc-900 w-full h-12 rounded-full flex items-center justify-between p-2 sticky bottom-0 left-0 sm:hidden">
-      <div className="h-10 w-10 rounded-full bg-zinc-800 flex justify-center items-center">
+    <div className="sticky bottom-4 left-0 bg-zinc-900 w-full h-auto rounded-full flex items-center justify-between p-1 sm:hidden z-10">
+      <div className="h-12 w-12 rounded-full bg-zinc-800 flex justify-center items-center">
         <HomeIcon className="h-6 w-6 text-white" />
       </div>
-      <div className="h-10 w-10 rounded-full bg-zinc-800 flex justify-center items-center">
+      <div className="h-12 w-12 rounded-full bg-zinc-800 flex justify-center items-center">
         <ArrowsRightLeftIcon className="h-6 w-6 text-white" />
       </div>
-      <div className="h-10 w-10 rounded-full bg-zinc-800 flex justify-center items-center">
+      <div className="h-12 w-12 rounded-full bg-zinc-800 flex justify-center items-center">
         <UserIcon className="h-6 w-6 text-white" />
       </div>
-      <div className="h-10 w-10 rounded-full bg-zinc-800 flex justify-center items-center">
+      <div className="h-12 w-12 rounded-full bg-zinc-800 flex justify-center items-center">
         <BellIcon className="h-6 w-6 text-white" />
       </div>
-      <div className="h-10 w-10 rounded-full bg-zinc-800 flex justify-center items-center">
+      <div className="h-12 w-12 rounded-full bg-zinc-800 flex justify-center items-center">
         <QrCodeIcon className="h-6 w-6 text-white" />
       </div>
     </div>
   );
 }
+
+// #endregion Functions (1)
