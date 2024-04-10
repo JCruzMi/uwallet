947cfcd586ba17d92e7507b6f0d6c052009a6e2c
Kiara <fernandezperezkiara@gmail.com>
2024-04-10 18:29:07 -0500

    fakeData

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,8 @@
 // #region Type aliases (1)
+import Format from "../utils/format";
 
 type Card = {
   name?: string;
-  title?: string;
   amount?: number;
   numWallet?: string;
   category?: string;
@@ -12,15 +12,15 @@ type Card = {
 
 // #region Functions (1)
 
-export default function Card({
-  name,
-  title,
-  amount,
-  numWallet,
-  category,
-}: Card) {
+export default function Card({ name, amount, numWallet, category }: Card) {
   return (
-    <div className="max-w-[230px] min-w-[230px] w-full min-h-[140px] rounded-lg bg-gradient-to-r from-pink-300 to-purple-500"></div>
+    <div className="max-w-[230px] min-w-[230px] w-full min-h-[140px] rounded-lg bg-gradient-to-r from-pink-300 to-purple-500 flex flex-col justify-between p-4 text-primary">
+      <div className="text-lg">
+        <div>{name}</div>
+        <div>{Format(amount)}</div>
+      </div>
+      <div>{numWallet}</div>
+    </div>
   );
 }
 
