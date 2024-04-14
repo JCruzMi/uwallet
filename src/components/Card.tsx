49832976704b7c001af00d8975f10c5950221304
Huan <juan.murato.cruz@gmail.com>
2024-04-14 17:18:46 -0500

    cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,25 +1,16 @@
 // #region Type aliases (1)
+import { Card as CardType } from "@/lib/definitions";
+
 import Format from "../utils/format";
 
-type Card = {
-  name?: string;
-  amount?: number;
-  numWallet?: string;
-  category?: string;
-};
-
-// #endregion Type aliases (1)
-
-// #region Functions (1)
-
-export default function Card({ name, amount, numWallet, category }: Card) {
+export default function Card({ name, amount, number }: CardType) {
   return (
     <div className="max-w-[230px] min-w-[230px] w-full min-h-[140px] rounded-lg bg-gradient-to-r from-pink-300 to-purple-500 flex flex-col justify-between p-4 text-primary">
       <div className="text-lg">
         <div>{name}</div>
         <div>{Format(amount)}</div>
       </div>
-      <div>{numWallet}</div>
+      <div>{number}</div>
     </div>
   );
 }
--- src/components/dashboard/TopNavigation.tsx
+++ src/components/dashboard/TopNavigation.tsx
@@ -1,19 +1,18 @@
-import Link from "next/link";
-
 import { ArrowLeftStartOnRectangleIcon } from "@heroicons/react/16/solid";
 
-import { signOut } from "../../../auth";
+import { auth, signOut } from "../../../auth";
 import Avatar from "../Avatar";
 import { Button } from "../ui/Button";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const session = await auth();
   return (
     <div className="sticky left-0 top-4 flex justify-between w-full z-10 bg-background">
       <div className="flex gap-4 flex-row">
         <Avatar />
         <div className="flex flex-col gap-0 h-full items-start justify-center">
           <p className="text-sm font-light">Good moorning!</p>
-          <p className="font-semibold">USER NAME</p>
+          <p className="font-semibold">{session?.user?.email}</p>
         </div>
       </div>
       <form
