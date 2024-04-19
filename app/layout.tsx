29c883ac37c194b95ce6f67996422bee80f766f9
Huan <juan.murato.cruz@gmail.com>
2024-04-19 14:38:34 -0500

    Revert "suuu"
    This reverts commit 61803d32891f7a65f6acc850ea641c9f91fc4ca0.

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,15 +17,18 @@ export default async function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={GeistSans.className}>
-        <main className="flex min-h-screen flex-col items-center justify-between ">
-          <div className="z-10 max-w-5xl flex-col w-full items-center justify-between font-mono text-sm lg:flex pt-4">
-            <div className="w-full">
-              <Navbar />
-            </div>
-            <SessionProvider session={session}>
-              <Providers>{children}</Providers>
-            </SessionProvider>
-          </div>
+      <main className="flex min-h-screen flex-col items-center justify-between ">
+      <div className="z-10 max-w-5xl flex-col w-full items-center justify-between font-mono text-sm lg:flex pt-4">
+        <div className="w-full">
+
+      <Navbar />
+        </div>
+        <SessionProvider session={session}>
+            <Providers>
+            {children}
+            </Providers>
+        </SessionProvider>
+        </div>
         </main>
       </body>
     </html>
