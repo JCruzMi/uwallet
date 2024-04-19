93bf3b242d1b38461461687be0705722c4e5f488
Huan <juan.murato.cruz@gmail.com>
2024-04-19 14:38:44 -0500

    Revert "."
    This reverts commit 96475feb3d6a620d0e48029357ea85b3823d8330.

--- utils/format.js
+++
@@ -1,8 +0,0 @@
-export default function Format(valor) {
-  return new Intl.NumberFormat("en-US", {
-    currency: "USD",
-    style: "currency",
-  })
-    .format(valor)
-    .replace("$", "$ ");
-}
