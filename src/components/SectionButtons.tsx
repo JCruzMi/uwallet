02d084ad8ed3fe6c0e2bf68a22641e8f8266c54c
Kiara <fernandezperezkiara@gmail.com>
2024-04-27 15:23:00 -0500

    button

--- src/components/SectionButtons.tsx
+++ src/components/SectionButtons.tsx
@@ -1,14 +1,39 @@
 import { Button } from "./ui/Button";
-
+import { CurrencyDollarIcon } from "@heroicons/react/24/outline";
+import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
+import { PlusIcon } from "@heroicons/react/24/outline";
+import { BanknotesIcon } from "@heroicons/react/24/outline";
 // #region Functions (1)
 
 export default function SectionButtons() {
+  const buttonClasses = "flex flex-col items-center";
+
   return (
-    <div className="flex gap-2 w-full max-w-sm">
-      <Button>Send</Button>
-      <Button>Receive</Button>
-      <Button>Withdraw</Button>
-      <Button>Others</Button>
+    <div className="flex justify-between w-full max-w-sm text-xs text-white">
+      <div className={buttonClasses}>
+        <Button>
+          <PaperAirplaneIcon className="h-4 w-4" />
+        </Button>
+        Send
+      </div>
+      <div className={buttonClasses}>
+        <Button>
+          <CurrencyDollarIcon className="h-4 w-4" />
+        </Button>
+        Receive
+      </div>
+      <div className={buttonClasses}>
+        <Button>
+          <BanknotesIcon className="h-4 w-4" />
+        </Button>
+        Withdraw
+      </div>
+      <div className={buttonClasses}>
+        <Button>
+          <PlusIcon className="h-4 w-4" />
+        </Button>
+        Others
+      </div>
     </div>
   );
 }
