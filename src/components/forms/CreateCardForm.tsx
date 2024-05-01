96fa505b2262760695cf86604391ba4b0368a2e2
Huan <juan.murato.cruz@gmail.com>
2024-05-01 16:15:38 -0500

    update

--- src/components/forms/CreateCardForm.tsx
+++ src/components/forms/CreateCardForm.tsx
@@ -15,21 +15,16 @@ export default function CreateCardForm() {
   } = useForm({
     defaultValues: {
       name: "",
-      number: "",
     },
   });
 
   const onSubmit = handleSubmit(async (data) => {
-    let obj = {
-      name: data.name.toString(),
-      number: data.number.toString(),
-    };
-    createCard(obj.name.toString(), obj.number.toString());
+    createCard(data.name.toString());
   });
   return (
     <form onSubmit={onSubmit} className="w-1/4">
       <label htmlFor="name" className="text-slate-500 mb-2 block text-sm">
-        Name
+        Name Card
       </label>
       <input
         type="text"
@@ -45,28 +40,6 @@ export default function CreateCardForm() {
 
       {errors.name && (
         <span className="text-red-500 text-xs">{errors.name.message}</span>
-      )}
-
-      <label
-        htmlFor="Number card"
-        className="text-slate-500 mb-2 block text-sm"
-      >
-        Number card
-      </label>
-      <input
-        type="text"
-        {...register("number", {
-          required: {
-            value: true,
-            message: "Number card is required",
-          },
-        })}
-        className="p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full"
-        placeholder="1000 1000 1000 1000"
-      />
-
-      {errors.number && (
-        <span className="text-red-500 text-xs">{errors.number.message}</span>
       )}
 
       <Button className="w-full rounded-lg">Submit</Button>
--- src/lib/cards.js
+++ src/lib/cards.js
@@ -1,25 +1,32 @@
 "use server";
 
-import { v4 as uuidv4 } from "uuid";
 import { NextResponse } from "next/server";
 import { sql } from "@vercel/postgres";
 import { auth } from "../../auth";
+import { generateCreditCard } from "./utils";
 
-export async function createCard(name, number) {
+export async function createCard(name) {
   const session = await auth();
-  const uid = uuidv4().toString();
-  if (session?.user) {
-    const userId = session.user.id.toString();
-    const amount = 0;
-    try {
-      const data =
-        await sql`insert into cards (id ,name, number, user_id, amount) values (${uid} ${name}, ${number}, ${userId}, ${amount})`;
+  try {
+    if (session?.user) {
+      const id = parseInt(generateCreditCard());
+      const card = generateCreditCard();
+      const userId = parseInt(session.user.id);
+      const amount = 0;
+      const created_at = new Date();
+      console.log(id, name, card, userId, amount, created_at);
+      const data = await sql`INSERT INTO
+         cards (id ,name, number, user_id, amount, created_at) VALUES 
+         (${id} ${name}, ${card}, ${userId}, ${amount}, ${created_at})`;
       console.log(data);
-    } catch (error) {
-      return NextResponse.json(error);
     }
+  } catch (error) {
+    return NextResponse.json(error);
   }
 }
+// insert into
+// cards (id ,name, number, user_id, amount, created_at)
+// values (75,'Juan David Cruz Miranda','7594 9262 4268 6415',23,0,'2024-05-01T20:43:53.000Z')
 
 export async function deleteCard(id) {
   try {
