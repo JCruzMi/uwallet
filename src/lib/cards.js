3426ee02fe2bbd2dbb09903db35c453472fc9360
Huan <juan.murato.cruz@gmail.com>
2024-05-02 16:39:48 -0500

    cards

--- src/lib/cards.js
+++ src/lib/cards.js
@@ -1,6 +1,5 @@
 "use server";
 
-import { NextResponse } from "next/server";
 import { sql } from "@vercel/postgres";
 import { auth } from "../../auth";
 import { generateCreditCard } from "./utils";
@@ -14,7 +13,6 @@ export async function createCard(name) {
       const userId = parseInt(session.user.id);
       const amount = 0;
       const created_at = new Date();
-      console.log(id, name, card, userId, amount, created_at);
       const data = await sql`INSERT INTO
          cards (id ,name, number, user_id, amount, created_at) VALUES 
          (${id}, ${name}, ${card}, ${userId}, ${amount}, ${created_at})`;
@@ -24,15 +22,12 @@ export async function createCard(name) {
     return res.status(400).json({ data: error });
   }
 }
-// insert into
-// cards (id ,name, number, user_id, amount, created_at)
-// values (75,'Juan David Cruz Miranda','7594 9262 4268 6415',23,0,'2024-05-01T20:43:53.000Z')
 
 export async function deleteCard(number_card) {
   try {
     await sql`delete from cards where number = ${number_card}`;
   } catch (error) {
-    return NextResponse.json(error);
+    return res.status(400).json({ data: error });
   }
 }
 
@@ -44,7 +39,7 @@ export async function getCards(user_id) {
     WHERE user_id = ${user_id}`;
     return data.rows;
   } catch (error) {
-    return NextResponse.json(error);
+    return res.status(400).json({ data: error });
   }
 }
 
@@ -53,7 +48,7 @@ export async function sendMoney(number_sender, number_receiver, amount) {
     await sql`update cards set amount = amount - ${amount} where number = ${number_sender}`;
     await sql`update cards set amount = amount + ${amount} where number = ${number_receiver}`;
   } catch (error) {
-    return NextResponse.json(error);
+    return res.status(400).json({ data: error });
   }
 }
 
@@ -61,7 +56,7 @@ export async function depositMoney(number, amount) {
   try {
     await sql`update cards set amount = amount + ${amount} where number = ${number}`;
   } catch (error) {
-    return NextResponse.json(error);
+    return res.status(400).json({ data: error });
   }
 }
 
@@ -69,14 +64,14 @@ export async function withdrawMoney(number, amount) {
   try {
     await sql`update cards set amount = amount - ${amount} where number = ${number}`;
   } catch (error) {
-    return NextResponse.json(error);
+    return res.status(400).json({ data: error });
   }
 }
 
-export async function updateCard(id, name, number) {
+export async function updateCard(id, name) {
   try {
-    `update cards set name = ${name}, number = ${number} where id = ${id}`;
+    `update cards set name = ${name} where id = ${id}`;
   } catch (error) {
-    return NextResponse.json(error);
+    return res.status(400).json({ data: error });
   }
 }
