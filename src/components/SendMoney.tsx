35364f45a0eb0c3962888aa63ac294b732e04bd0
Kiara <fernandezperezkiara@gmail.com>
2024-05-03 23:01:41 -0500

    fixed card

--- src/components/SendMoney.tsx
+++ src/components/SendMoney.tsx
@@ -18,7 +18,7 @@ import SendMoneyForm from "./forms/SendMoneyForm";
 export default function SendMoney({ numberSender, amountCard }: { numberSender: string; amountCard: number }) {
   return (
     <Drawer>
-      <DrawerTrigger className="flex gap-2 items-center p-1 text-sm">
+      <DrawerTrigger className="flex gap-2 items-center px-1 py-2 transition-colors hover:bg-zinc-900 rounded-md text-sm w-full">
         <div className="text-sm flex items-center justify-center h-5 w-5 rounded-full bg-white bg-opacity-10 transition-colors hover:bg-opacity-15">
           <PaperAirplaneIcon className="h-3 w-3" />
         </div>
--- src/components/WithdrawMoney.tsx
+++ src/components/WithdrawMoney.tsx
@@ -18,7 +18,7 @@ import WithdrawMoneyForm from "./forms/WithdrawMoney";
 export default function WithdrawMoney({ number, amount}: { number: string, amount: number}) {
   return (
     <Drawer>
-      <DrawerTrigger className="flex gap-2 items-center p-1 text-sm">
+      <DrawerTrigger className="flex gap-2 items-center px-1 py-2 transition-colors hover:bg-zinc-900 rounded-md text-sm w-full">
         <div className="text-sm flex items-center justify-center h-5 w-5 rounded-full bg-white bg-opacity-10 transition-colors hover:bg-opacity-15">
           <BanknotesIcon className="h-3 w-3" />
         </div>
@@ -27,7 +27,7 @@ export default function WithdrawMoney({ number, amount}: { number: string, amoun
       <DrawerContent>
         <DrawerHeader className="w-1/4 px-1 flex justify-start flex-col items-start mx-auto">
           <DrawerTitle>Withdraw Money</DrawerTitle>
-          <DrawerDescription>Have your money from the card at your hand</DrawerDescription>
+          <DrawerDescription>Have your money from the card at your hand.</DrawerDescription>
         </DrawerHeader>
         <DrawerFooter className="flex items-center justify-center">
           <WithdrawMoneyForm number={number} amount={amount} />
