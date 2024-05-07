cb4339adeb711f44c32e27434951299c77274b1c
Huan <juan.murato.cruz@gmail.com>
2024-05-06 20:07:41 -0500

    movements

--- src/components/Movements.tsx
+++ src/components/Movements.tsx
@@ -25,6 +25,32 @@ export default function Movements() {
       });
   }, []);
 
+  const iconType = (type: string) => {
+    switch (type) {
+      case "draw":
+        return (
+          <div className="flex items-center gap-2">
+            <BanknotesIcon className="h-5 w-5" />
+            <p className="text-sm">Draw</p>
+          </div>
+        );
+      case "deposit":
+        return (
+          <div className="flex items-center gap-2">
+            <CurrencyDollarIcon className="h-5 w-5" />
+            <p className="text-sm">Deposit</p>
+          </div>
+        );
+      case "transfer":
+        return (
+          <div className="flex items-center gap-2">
+            <PaperAirplaneIcon className="h-5 w-5" />
+            <p className="text-sm">Transfer</p>
+          </div>
+        );
+    }
+  };
+
   if (loading) {
     return (
       <p className="animate-pulse w-[200px] h-8 rounded-md bg-secondary"></p>
@@ -38,7 +64,15 @@ export default function Movements() {
           className="flex flex-col gap-2 bg-zinc-800 p-4 rounded-lg w-full"
         >
           <p></p>
+          <p className="text-2xl font-semibold">{iconType(item.type)}</p>
           <p className="text-2xl font-semibold">{Format(item.amount)}</p>
+          {item.deposit !== "true" && (
+            <p className="text-sm font-light">{item.number_sender}</p>
+          )}
+          {item.draw !== "true" && (
+            <p className="text-sm font-light">{item.number_receiver}</p>
+          )}
+
           <p className="text-sm font-light">
             {new Date(item.created_at).toLocaleString()}
           </p>
