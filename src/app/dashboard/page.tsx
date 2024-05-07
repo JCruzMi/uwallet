66a470228a5ffafe36f126f6cee38b6abbb1c276
Huan <juan.murato.cruz@gmail.com>
2024-05-07 18:16:06 -0500

    better navbar login

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,29 +9,31 @@ import SliderCards from "@/components/SliderCards";
 
 const Dashboard = async () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between lg:px-24 p-4 w-full relative">
-      <div className="z-10 max-w-5xl w-full items-start justify-start font-mono text-sm gap-4 flex flex-col relative">
-        <TopNavigation />
-        <div className="flex flex-col gap-0 py-4">
-          <p className="text-sm font-light">Total Balance</p>
-          <Balance />
+    <>
+      <TopNavigation />
+      <main className="flex min-h-screen flex-col items-center justify-between lg:px-24 p-4 py-0 w-full relative">
+        <div className="z-10 max-w-5xl w-full items-start justify-start font-mono text-sm gap-4 flex flex-col relative pb-8 sm:pb-0">
+          <div className="flex flex-col gap-0 py-4">
+            <p className="text-sm font-light">Total Balance</p>
+            <Balance />
+          </div>
+          <div className="flex flex-row w-full gap-4 max-h-[140px] overflow-hidden">
+            <SliderCards />
+          </div>
+          <div className="flex justify-between w-full">
+            <h1>Send money</h1>
+            <h1>Find one</h1>
+          </div>
+          <SectionSend />
+          <div className="flex justify-between w-full">
+            <h1>Movements</h1>
+            <h1>View all</h1>
+          </div>
+          <Movements />
         </div>
-        <div className="flex flex-row w-full gap-4 max-h-[140px] overflow-hidden">
-          <SliderCards />
-        </div>
-        <div className="flex justify-between w-full">
-          <h1>Send money</h1>
-          <h1>Find one</h1>
-        </div>
-        <SectionSend />
-        <div className="flex justify-between w-full">
-          <h1>Movements</h1>
-          <h1>View all</h1>
-        </div>
-        <Movements />
-      </div>
-      <Navbar />
-    </main>
+        <Navbar />
+      </main>
+    </>
   );
 };
 
--- src/components/dashboard/Navbar.tsx
+++ src/components/dashboard/Navbar.tsx
@@ -10,7 +10,7 @@ import {
 
 export default function Navbar() {
   return (
-    <div className="sticky bottom-4 left-0 bg-zinc-900 w-full h-auto rounded-full flex items-center justify-between p-1 sm:hidden z-10">
+    <div className="sticky bottom-4 left-0 bg-zinc-900/60 backdrop-blur-sm w-full h-auto rounded-full flex items-center justify-between p-1 sm:hidden z-10">
       <div className="h-12 w-12 rounded-full bg-zinc-800 flex justify-center items-center">
         <HomeIcon className="h-6 w-6 text-white" />
       </div>
