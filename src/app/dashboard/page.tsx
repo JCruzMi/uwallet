0590c5d43a2d2d3a09f7bddca54757e249cd60e3
Huan <juan.murato.cruz@gmail.com>
2024-05-17 12:14:44 -0500

    cambios mamalos

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,18 +10,20 @@ const Dashboard = async () => {
   return (
     <>
       <TopNavigation />
-      <main className="flex flex-col items-center justify-between lg:px-24 p-4 py-0 w-full relative">
+      <main className="flex flex-col items-center justify-between lg:px-24 p-4 py-0 w-full relative pt-16">
         <div className="z-10 max-w-5xl w-full items-start justify-start font-mono text-sm gap-4 flex flex-col relative pb-8 sm:pb-4">
           <div className="flex flex-col gap-0 py-4">
-            <p className="text-sm font-light">Total Balance</p>
+            <p className="text-sm font-semibold">Total Balance</p>
             <Balance />
           </div>
           <div className="flex flex-row w-full gap-4 max-h-[140px] overflow-hidden">
             <SliderCards />
           </div>
           <div className="flex justify-between w-full">
-            <h1>Movements</h1>
-            <h1>View all</h1>
+            <h2 className="font-semibold">Movements</h2>
+            <p className="hover:underline transition-all cursor-pointer">
+              View all
+            </p>
           </div>
           <Movements />
         </div>
---
+++ src/utils/copyText.ts
@@ -0,0 +1,8 @@
+// #region Functions (1)
+
+//TODO: añadir toast de que copio el texto
+export default function copyText(text: string) {
+    navigator.clipboard.writeText(text);
+}
+
+// #endregion Functions (1)
