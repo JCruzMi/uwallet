d1443a6cb888d7ce6cd0d0901257775742acb40d
Huan <juan.murato.cruz@gmail.com>
2024-05-18 11:39:54 -0500

    scroll

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="px-4 lg:px-0 lg:container">
+    <div className="px-4 max-w-5xl mx-auto w-full">
       <div className="flex flex-col lg:flex-row justify-between items-center py-6 gap-y-6 lg:gap-y-6">
         <p className="text-[#5F7896] text-[16px] font-medium text-center lg:text-left">
           © Copyright 2024. Your Site. All rights reserved.
