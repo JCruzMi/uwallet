ebaa238b12cd9d6cdfc9bceffbaf15cd4de280be
Huan <juan.murato.cruz@gmail.com>
2024-05-18 14:20:28 -0500

    --path

--- src/components/auth/layout.tsx
+++ src/components/auth/layout.tsx
@@ -1,11 +1,12 @@
 "use client";
 import Link from "next/link";
 import { ReactNode } from "react";
+
 import { Button } from "@/components/ui/Button";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="relative h-[100svh]  flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-zinc-900" />
         <div className="relative flex items-center text-lg font-medium gap-3 w-auto">
